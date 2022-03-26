54ca1445702912bd2ca4a0a9fdfeed4dbd78c332
Tobias Mücksch <tobias@muecks.ch>
2022-03-26 20:17:00 +0100

    Fixes from issue
    https://github.com/capacitor-community/sqlite/issues/244#issuecomment-1079748706

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -10,7 +10,9 @@ import { SqliteService } from './services/sqlite.service';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SqliteService],
-  bootstrap: [AppComponent]
+  providers: [SqliteService,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SqliteService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
--- src/app/services/sqlite.service.ts
+++ src/app/services/sqlite.service.ts
@@ -13,53 +13,79 @@ export class SqliteService {
 
   constructor() { }
 
+
   async init() {
     console.log('Creating Connection', `Platform is: ${Capacitor.getPlatform()}`);
     // Create a connection
     this.serviceConnection = new SQLiteConnection(CapacitorSQLite);
-    console.log('creating connection');
-    this.dbConnection = await this.serviceConnection.createConnection('products', false, 'no-encryption', 1);
-    console.log('CONNECTION IS', this.dbConnection);
-
-    // Not implemented on web??
-    //await CapacitorSQLite.open({ database: 'bico' });
 
     console.log('Connection is', this.serviceConnection);
 
     if (Capacitor.getPlatform() === 'web') {
-
       await customElements.whenDefined('jeep-sqlite');
 
       const jeepSqliteEl = document.querySelector('jeep-sqlite');
       if (jeepSqliteEl != null) {
         await this.initWebStore();
-
-
-        console.log(`isStoreOpen ${await jeepSqliteEl.isStoreOpen()}`)
-        console.log(`$$ jeepSqliteEl is defined}`);
+        console.log(`isStoreOpen ${await jeepSqliteEl.isStoreOpen()}`);
+        console.log(`$$ jeepSqliteEl is defined`);
       } else {
         console.log('$$ jeepSqliteEl is null');
       }
     }
   }
-
   async initDB() {
-    await CapacitorSQLite.isJsonValid({ jsonstring: productSchemaJson });
+    try {
+      const result = (await CapacitorSQLite.isJsonValid({ jsonstring: productSchemaJson })).result;
+      if (result) {
+        const ret = await this.serviceConnection.importFromJson(productSchemaJson);
+        if (ret.changes.changes === -1) {
+          console.log(`initDB: import Json Object failed`);
+        }
+      } else {
+        console.log(`initDB: Json Object not valid`);
+      }
+    } catch (err) {
+      console.log(`initDB: ${err}`);
+    }
+  }
+  async openDB(): Promise<boolean> {
+    try {
+      const retCC = (await this.serviceConnection.checkConnectionsConsistency()).result;
+      const isConn = (await this.serviceConnection.isConnection('product-db')).result;
+      if (retCC && isConn) {
+        this.dbConnection = await this.serviceConnection.retrieveConnection('product-db');
+      } else {
+        this.dbConnection = await this.serviceConnection
+          .createConnection('product-db', false, 'no-encryption', 1);
+      }
+      await this.dbConnection.open();
+      return true;
+    } catch (err) {
+      console.log(`openDB: ${err}`);
+      return false;
+    }
 
-    this.serviceConnection.importFromJson(productSchemaJson);
   }
 
   async printAllDbs() {
-    console.log('DB LIST', await this.serviceConnection.getDatabaseList());
+    console.log('DB LIST', (await this.serviceConnection.getDatabaseList()).values);
   }
 
   async printQuery() {
-    const statement = 'SELECT * FROM products;';
-    const values = [];
+    try {
+      const isOpen = await this.openDB();
+      if (isOpen) {
+        const statement = 'SELECT * FROM products;';
+        const values = [];
+        const result = await this.dbConnection.query(statement, values);
+        console.log('result', result.values);
+      }
 
-    const result = await CapacitorSQLite.query({ statement, values, database: 'products' });
+    } catch (err) {
+      console.log(`printQuery: ${err}`);
+    }
 
-    console.log('result', result);
   }
 
   async test() {
